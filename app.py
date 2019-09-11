86ac43ef109a23345371db694c2cd0e5c39699fa
Kleina13 <ragnarhelgi22@gmail.com>
2019-09-11 17:08:38 +0000

    for loop :)

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ from flask import Flask, render_template as rend
 
 app = Flask(__name__)
 
-lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem mollis aliquam ut porttitor leo a. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Nisl nunc mi ipsum faucibus vitae aliquet. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Arcu ac tortor dignissim convallis aenean et tortor. Aliquam vestibulum morbi blandit cursus risus at. Feugiat in ante metus dictum at tempor commodo ullamcorper. Magnis dis parturient montes nascetur ridiculus mus. Sed libero enim sed faucibus turpis in eu. Sed tempus urna et pharetra. Tincidunt vitae semper quis lectus nulla at. Blandit massa enim nec dui nunc mattis enim. Amet commodo nulla facilisi nullam. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Sodales ut eu sem integer. Viverra suspendisse potenti nullam ac tortor vitae purus. Sed risus pretium quam vulputate dignissim. Lobortis feugiat vivamus at augue eget arcu dictum. Pellentesque sit amet porttitor eget.'
+lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem mollis aliquam ut porttitor leo a. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum. Nisl nunc mi ipsum faucibus vitae aliquet. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat. Arcu ac tortor dignissim convallis aenean et tortor. Aliquam vestibulum morbi blandit cursus risus at. Feugiat in ante metus dictum at tempor commodo ullamcorper. Magnis dis parturient montes nascetur ridiculus mus. Sed libero enim sed faucibus turpis in eu. Sed tempus urna et pharetra. Tincidunt vitae semper quis lectus nulla at. Blandit massa enim nec dui nunc mattis enim. Amet commodo nulla facilisi nullam. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Sodales ut eu sem integer. Viverra suspendisse potenti nullam ac tortor vitae purus. Sed risus pretium quam vulputate dignissim. Lobortis feugiat vivamus at augue eget arcu dictum. Pellentesque sit amet porttitor eget. Scelerisque eu ultrices vitae auctor eu augue ut lectus. Euismod elementum nisi quis eleifend quam adipiscing. Suspendisse ultrices gravida dictum fusce ut placerat orci. Sed odio morbi quis commodo odio aenean sed adipiscing. Rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Bibendum ut tristique et egestas quis ipsum suspendisse ultrices gravida. At lectus urna duis convallis convallis. Adipiscing bibendum est ultricies integer. Pharetra vel turpis nunc eget. In massa tempor nec feugiat nisl. Lobortis feugiat vivamus at augue eget arcu dictum varius. Phasellus vestibulum lorem sed risus ultricies tristique nulla. Sed augue lacus viverra vitae.'
 
-articles = [{'id': 1, 'author': 'Guðrún', 'title': 'epic', 'content': lorem_ipsum},
-			{'id': 2, 'author': 'Arnór', 'title': 'car', 'content': lorem_ipsum},
-			{'id': 3, 'author': 'Erik', 'title': 'battle', 'content': lorem_ipsum}]
+articles = [{'id': 1, 'author': "Noami Winters", 'title': "Rat poison in school's caffettiera food", 'content': lorem_ipsum},
+			{'id': 2, 'author': "Sebastian Sanchez", 'title': "Video games bad!", 'content': lorem_ipsum},
+			{'id': 3, 'author': "Samuel Jackson", 'title': "Donald Trump commits genocide", 'content': lorem_ipsum}]
 author, title, content = '', '', ''
+lenA = len(articles)
 
 @app.route('/')
 def index():
@@ -22,7 +23,7 @@ def article(ID):
 			author = articles[x]['author']
 			title = articles[x]['title']
 			content = articles[x]['content']
-	return rend('layout.html', author=author, title=title, content=content)
+	return rend('layout.html', author=author, title=title, content=content, articles=articles, len=lenA)
 
 
 if __name__ == "__main__":
